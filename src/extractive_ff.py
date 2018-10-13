43306456a7e28ea7ee9e60f41ef12f452df4d860
Mauricio Steinert <mauricio.steinert@gmail.com>
2018-10-13 19:23:15 -0300

    Small adjustments to feed-forward nn

--- src/extractive_ff.py
+++ src/extractive_ff.py
@@ -23,6 +23,7 @@ common.log_message("INFO", "\n")
 common.get_test()
 
 n_hidden_1 = common.test_x.shape[0]
+n_hidden_2 = 1000
 
 with tf.device(device):
     x_ = tf.placeholder(tf.float32, shape=[None, common.test_x.shape[1]])
@@ -30,16 +31,19 @@ with tf.device(device):
 
     weights = {
         'h1': tf.Variable(tf.random_normal([common.test_x.shape[1], n_hidden_1], seed=random_seed)),
-        'out': tf.Variable(tf.random_normal([n_hidden_1, common.test_y.shape[1]], seed=random_seed))
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], seed=random_seed)),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, common.test_y.shape[1]], seed=random_seed))
     }
 
     biases = {
         'b1': tf.Variable(np.zeros([n_hidden_1], np.float32)),
+        'b2': tf.Variable(np.zeros([n_hidden_2], np.float32)),
         'out': tf.Variable(np.zeros(common.test_y.shape[1], np.float32))
     }
 
     layer_1 = tf.add(tf.matmul(x_, weights['h1']), biases['b1'])
-    out_layer = tf.add(tf.matmul(layer_1, weights['out']), biases['out'])
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    out_layer = tf.add(tf.matmul(layer_2, weights['out']), biases['out'])
 
     loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
         logits=out_layer, labels=y_))
@@ -74,8 +78,12 @@ with tf.device(device):
                 loss_test,acc_test = sess.run([loss_op, accuracy],
                     feed_dict={x_: common.test_x, y_: common.test_y})
 
-                print("[" + str(curr_epoch) + "] loss = "
+                common.log_message("INFO", "[" + str(curr_epoch) + "] loss = "
                         + str(loss) + "\tacc = " + str(acc)
                         + "\ttest loss = " + str(loss_test)
                         + "\ttest acc = " + str(acc_test))
+                # print("[" + str(curr_epoch) + "] loss = "
+                #         + str(loss) + "\tacc = " + str(acc)
+                #         + "\ttest loss = " + str(loss_test)
+                #         + "\ttest acc = " + str(acc_test))
                 # save_path = saver.save(sess, "./checkpoint_" + str(curr_step) + ".ckpt")
